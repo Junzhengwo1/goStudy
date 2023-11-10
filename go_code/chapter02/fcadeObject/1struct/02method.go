cc0ec391bcf162b168902a7b79d603d3947c9c1e
koujiajun <2627084538@qq.com>
2023-11-10 16:08:01 +0800

    学习篇

--- go_code/chapter02/fcadeObject/1struct/02method.go
+++ go_code/chapter02/fcadeObject/1struct/02method.go
@@ -19,16 +19,18 @@ type P struct {
 // 表达了该方法唯一属于 Pes 要想使用该方法 则需要通过 s.Pes.read()
 func (s Pes) read() {
 	fmt.Println(s.name + "学生正在读书……")
+	fmt.Println(s.age)
+	s.age = 10000
+	fmt.Println(s.age)
 }
 
 func main() {
 	p := Pes{22, "que", 23}
-
 	s := P{Pes{2, "张三", 23}, "aksdia"}
 
 	fmt.Println(p.name)
 	p.read() // 对象调用方法 对象自己相当于也是传递了自己 函数属于包
 	fmt.Println(s)
 	s.read() // 继承了 Pes 所以可以直接调用 Pes 的方法
-
+	fmt.Println(s)
 }
