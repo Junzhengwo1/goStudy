c4d5a3504065fab33efb8afa967aec8d83414576
kou <soslovemiss@ding.com>
2023-11-12 19:35:13 +0800

    aa

--- go_code/chapter03/1xiecheng/myXiecheng.go
+++ go_code/chapter03/1xiecheng/myXiecheng.go
@@ -2,6 +2,8 @@ package _xiecheng
 
 /**
 协程 本质还是单线程的
+
+主死从随
 */
 
 func myXieCheng() {
--- go_code/chapter03/main.go
+++ go_code/chapter03/main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var waitGroup sync.WaitGroup
+
 func main() {
 
-	go test() // 开启协程
-	for i := 0; i < 10; i++ {
-		fmt.Println("main ", i)
-		time.Sleep(time.Second)
+	// 主死从随
+	//go test() // 开启协程
+	//for i := 0; i < 10; i++ {
+	//	fmt.Println("main ", i)
+	//	time.Sleep(time.Second)
+	//
+	//}
 
-	}
+	// 第二种方式 启动协程  匿名函数的方式
+	//for i := 0; i < 10; i++ {
+	//	go func(n int) {
+	//		fmt.Println("go ", n)
+	//	}(i)
+	//}
+	//time.Sleep(time.Second)
+
+	// waitGroup := sync.WaitGroup{} 来管理协程
+	testWaitGroup()
+
+	test2()
 
 }
 
@@ -22,4 +39,29 @@ func test() {
 		fmt.Println(i)
 		time.Sleep(time.Second)
 	}
+}
+
+func testWaitGroup() {
+	for i := 0; i < 10; i++ {
+		waitGroup.Add(1)
+		go func(n int) {
+			fmt.Println("go ", n)
+			waitGroup.Done()
+		}(i)
+	}
+	waitGroup.Wait()
+}
+
+// 多个协程操作同一个变量
+func test2() {
+	var num int
+	for i := 0; i < 10; i++ {
+		waitGroup.Add(1)
+		go func(n int) {
+			num = n
+			waitGroup.Done()
+		}(i)
+	}
+	waitGroup.Wait()
+	fmt.Println(num) // 理论上应该是 9
 }
