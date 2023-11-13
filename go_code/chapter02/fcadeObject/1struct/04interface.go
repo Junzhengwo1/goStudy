eb0f97f877dab4489b93d34c365d545c26842f51
koujiajun <2627084538@qq.com>
2023-11-13 09:25:25 +0800

    学习篇

--- go_code/chapter02/fcadeObject/1struct/04interface.go
+++ go_code/chapter02/fcadeObject/1struct/04interface.go
@@ -1,6 +1,7 @@
 package _struct
 
 // 接口的定义
+// 接口里面不能有任何变量
 // 接口的定义是一种类型，它定义了一种行为集合，接口的定义包含了接口的名称和方法的集合
 // 接口的定义包含了接口的名称和方法的集合
 // 一个结构体可以实现多个接口
@@ -17,6 +18,10 @@ type A struct {
 
 func (a A) sayHello() {
 	println("你好")
+}
+
+func (a A) Ni() {
+	println("你好2")
 }
 
 type B struct {
--- go_code/chapter02/main.go
+++ go_code/chapter02/main.go
@@ -16,4 +16,6 @@ func main() {
 
 	_struct.Greet(b)
 
+	a.Ni()
+
 }
