c1404222263d75d5205fd33b4955d72db99c935b
koujiajun <2627084538@qq.com>
2023-11-15 17:57:21 +0800

    学习篇-go-gin-gorm-project 规范 go项目的项目结构风格

--- go_xproject_chat/dao/user_dao.go
+++ go_xproject_chat/dao/user_dao.go
@@ -2,8 +2,8 @@ package dao
 
 import (
 	"fmt"
-	"goStudy/go_xproject_chat/config"
 	"goStudy/go_xproject_chat/model/pojo"
+	"goStudy/go_xproject_chat/util"
 )
 
 type UserDao struct {
@@ -12,7 +12,7 @@ type UserDao struct {
 func (*UserDao) Query(id int) pojo.User {
 	// 查询单条数据
 	var user pojo.User
-	err := config.Db.Where("id = ?", id).First(&user).Error
+	err := util.Db.Where("id = ?", id).First(&user).Error
 	if err != nil {
 		fmt.Println(err)
 	}
---
+++ go_xproject_chat/util/system_init.go
@@ -0,0 +1,3 @@
+package util
+
+// 系统出初始化
