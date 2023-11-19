9db6cd63d14c241fc8e73c64da871ee22a4d49ba
koujiajun <2627084538@qq.com>
2023-11-19 13:37:58 +0800

    学习篇-go-grpc-demo

--- go_x_rpc/grpc/client/main.go
+++ go_x_rpc/grpc/client/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"context"
+	pb "goStudy/go_x_rpc/grpc/client/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
 func main() {
+
+	conent, _ := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	defer conent.Close()
+	client := pb.NewSayHelloClient(conent)
+	log.Println("client start")
+	hello, _ := client.SayHello(context.Background(), &pb.HelloRequest{Name: "king"})
+	//
+	log.Println("从服务端拿到的返回结果：", hello.GetMessage())
 
 }
--- go_x_rpc/grpc/server/main.go
+++ go_x_rpc/grpc/server/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"context"
+	pb "goStudy/go_x_rpc/grpc/server/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type MyServer struct {
+	pb.UnimplementedSayHelloServer
+}
+
+func (*MyServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResp, error) {
+
+	log.Println("客户端的请求：", req.Name)
+	// 提供出去的 api
+	return &pb.HelloResp{
+		Message: "hello " + req.Name,
+		A:       1,
+	}, nil
+
+}
+
 func main() {
+
+	// 启动服务端
+	listen, _ := net.Listen("tcp", ":9090")
+
+	server := grpc.NewServer()
+	pb.RegisterSayHelloServer(server, &MyServer{})
+	if err := server.Serve(listen); err != nil {
+		panic(err)
+	}
+	log.Println("server start")
 
 }
